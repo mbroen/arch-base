e05b77975a0cbbedfd17e55d6c7b3418cdb6e96b
Mads Broen Nielsen <mail@mbroen.com>
2018-08-02 17:52:17 +0200

    merge build and clean-up. remove tini. add supervisor killer

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,12 @@ FROM base/archlinux
 
 ADD build/*.sh /root/
 ADD build/supervisor.conf /etc/supervisor.conf
+ADD build/bin/* /usr/local/bin/
 
-RUN chmod +x /root/*.sh && \
-    cd /root/ && \
-    /bin/bash /root/build.sh
+RUN chmod +x /usr/local/bin/* && \
+    /usr/local/bin/docker-build
 
 ENV HOME /home/nobody
 ENV LANG en_GB.UTF-8
 
-ENTRYPOINT ["/usr/bin/tini", "--"]
+ENTRYPOINT ["/usr/bin/supervisord", "-c", "/etc/supervisor.conf", "-n"]
--- build/00-packages.sh
+++ build/00-packages.sh
@@ -44,7 +44,7 @@ grep -v xfsprogs) \
 pacman -S git awk sed supervisor vi openssl moreutils --noconfirm --needed
 
 
-curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/tini_release_tag -L https://github.com/krallin/tini/releases
-tini_release_tag=$(cat /tmp/tini_release_tag | grep -P -o -m 1 '(?<=/krallin/tini/releases/tag/)[^"]+')
-curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /usr/bin/tini -L "https://github.com/krallin/tini/releases/download/${tini_release_tag}/tini-amd64" && chmod +x /usr/bin/tini
+#curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/tini_release_tag -L https://github.com/krallin/tini/releases
+#tini_release_tag=$(cat /tmp/tini_release_tag | grep -P -o -m 1 '(?<=/krallin/tini/releases/tag/)[^"]+')
+#curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /usr/bin/tini -L "https://github.com/krallin/tini/releases/download/${tini_release_tag}/tini-amd64" && chmod +x /usr/bin/tini
 

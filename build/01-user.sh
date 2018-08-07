0ff44f31bac80f010ba33b75f793a97224ce1f24
Mads Broen Nielsen <mail@mbroen.com>
2018-08-07 23:07:59 +0200

    add aur user

--- build/01-user.sh
+++ build/01-user.sh
@@ -11,3 +11,11 @@ chmod -R 775 /home/nobody
 
 usermod -d /home/nobody nobody
 chsh -s /bin/bash nobody
+
+# Add AUR user
+AUR_USER=aur
+useradd -m $AUR_USER
+# Set user password to blank
+echo "${AUR_USER}:" | chpasswd -e
+
+echo "$AUR_USER      ALL = NOPASSWD: ALL" >> /etc/sudoers.d/aur
